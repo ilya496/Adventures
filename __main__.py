84c63514fc4f79fda8a30972c6db1ec8b822d0b4
Ilja <birukov0608@gmail.com>
2021-01-15 15:39:35 +0300

    Управление на WASD, загрузил спрайт главного персонаж

--- __main__.py
+++ __main__.py
@@ -1,14 +1,20 @@
-import pygame, player
+import pygame
+import player
+import settings
 
 pygame.init()
-screen = pygame.display.set_mode([600, 400])
+screen = pygame.display.set_mode([settings.WIDTH, settings.HEIGHT])
+clock = pygame.time.Clock()
+pygame.key.set_repeat(50)
 
 player = player.Player()
 all_sprites = pygame.sprite.Group()
 all_sprites.add(player)
 
 while True:
-    pygame.event.get()
+    # pygame.event.get()
+    screen.fill([0, 0, 0])
+    player.process_events()
     all_sprites.draw(screen)
     pygame.display.flip()
-
+    clock.tick(60)
--- player.py
+++ player.py
@@ -1,4 +1,5 @@
 import pygame
+from settings import PLAYER_STEP
 
 
 class Player(pygame.sprite.Sprite):
@@ -6,10 +7,36 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
 
-        self.image = pygame.Surface([50, 50])
+        self.image = pygame.image.load('resources/pictures/main_player.png').convert_alpha()
         self.rect = pygame.rect.Rect(100, 100, self.image.get_width(), self.image.get_height())
+        self.flipped_right = True
+        self.flipped_left = False
 
-        self.image.fill([0, 255, 255])
+
+    def move(self, key):
+
+        if key == pygame.K_w:
+            self.rect.y -= PLAYER_STEP
+
+        elif key == pygame.K_s:
+            self.rect.y += PLAYER_STEP
+
+        elif key == pygame.K_d:
+            self.rect.x += PLAYER_STEP
+            if self.flipped_left:
+                self.image = pygame.transform.flip(self.image, True, False)
+                self.flipped_right = True
+                self.flipped_left = False
+
+        elif key == pygame.K_a:
+            self.rect.x -= PLAYER_STEP
+            if self.flipped_right:
+                self.image = pygame.transform.flip(self.image, True, False)
+                self.flipped_right = False
+                self.flipped_left = True
 
 
-
+    def process_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                self.move(event.key)
